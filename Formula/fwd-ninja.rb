59da026656212c255b0e61901146d7bc1f508284
Gary <garyberger@forwardnetworks.com>
2020-03-24 12:26:07 -0400

    change release target
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-ninja.rb
+++ Formula/fwd-ninja.rb
@@ -1,5 +1,5 @@
 class FwdNinja < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
-  url  "https://gerrit.local.forwardnetworks.com/fwd-ninja/ninja"
+  url  "https://github.com/gaberger/fwd-ninja/releases/download/0.0.32/ninja"
   sha256 "60FD9539319D9A3730C5B3C80BA138CDA8B9FAD302AE0BC3B812BEF2280F8E03"
 end
