beec4910237d3e722d8a021e421658e5c0c019d6
Gary <garyberger@forwardnetworks.com>
2020-03-24 12:55:35 -0400

    fix sha
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-ninja.rb
+++ Formula/fwd-ninja.rb
@@ -1,5 +1,5 @@
 class FwdNinja < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
   url  "https://github.com/gaberger/fwd-ninja/releases/download/0.0.32/ninja"
-  sha256 "C901FE59FFEB3F73D43B94C06FDF5853FC3C4717C817865338B935F8B8C92429"
+  sha256 "969b53f181c335ded2d97d5e8b583c37bed6412b57087ce0b3707a49a850f0aa"
 end
