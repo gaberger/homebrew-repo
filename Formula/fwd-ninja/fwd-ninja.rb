1524a92613574eace3f332638edb1a745fecb86b
Gary <garyberger@forwardnetworks.com>
2020-03-24 12:07:54 -0400

    Commit fwd-ninja
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

---
+++ Formula/fwd-ninja/fwd-ninja.rb
@@ -0,0 +1,5 @@
+class FwdNinja < Formula
+  homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
+  url  "https://gerrit.local.forwardnetworks.com/fwd-ninja/ninja"
+  sha256 "60FD9539319D9A3730C5B3C80BA138CDA8B9FAD302AE0BC3B812BEF2280F8E03"
+end
