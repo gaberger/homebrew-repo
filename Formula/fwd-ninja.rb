ced151608f3c5003b85fcbe5d926c1af12768178
Gary <garyberger@forwardnetworks.com>
2020-03-24 13:12:54 -0400

    Add installer
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-ninja.rb
+++ Formula/fwd-ninja.rb
@@ -2,4 +2,15 @@ class FwdNinja < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
   url  "https://github.com/gaberger/fwd-ninja/releases/download/0.0.32/ninja"
   sha256 "969b53f181c335ded2d97d5e8b583c37bed6412b57087ce0b3707a49a850f0aa"
+
+  bottle :unneeded
+
+  def install
+    bin.install "ninja"
+  end
+
+  test do
+    system "#{bin}/ninja", "--help"
+  end
+
 end
