04cec33c7337548f1b30d96e4b9050a7f4757430
Gary <garyberger@forwardnetworks.com>
2020-03-26 17:48:28 -0400

    Update class
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-nqe.rb
+++ Formula/fwd-nqe.rb
@@ -1,4 +1,4 @@
-class FwdNinja < Formula
+class FwdNqe < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/nqe"
   url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.1/nqe"
   sha256 "b1993d74bc056b1c3781838cc06864dace32a31fdd939fabf457f04e3a690aec"
