2a1e8de87935283b139c697bfa3faeab0d39af56
Gary <garyberger@forwardnetworks.com>
2020-03-30 12:02:05 -0400

    Version 0.0.7 fix nqe detection
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-nqe.rb
+++ Formula/fwd-nqe.rb
@@ -1,7 +1,7 @@
 class FwdNqe < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/nqe"
-  url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.6/nqe"
-  sha256 "2eb5cea04c4efef2722b18d102c3dcd4e41ba9c65cb4f4f06b7421e822e62367"
+  url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.7/nqe"
+  sha256 "b0456f0f97d0db6b52535fffc9fe309a854e70904e73f5241350e61f094e64ba"
 
   bottle :unneeded
 
