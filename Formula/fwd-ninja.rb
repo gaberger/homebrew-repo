3780101f264f76e349537d9562f7188aabc42510
Gary <garyberger@forwardnetworks.com>
2020-04-21 12:14:42 -0400

    Update 0.34
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-ninja.rb
+++ Formula/fwd-ninja.rb
@@ -1,7 +1,7 @@
 class FwdNinja < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
-  url  "https://github.com/gaberger/fwd-ninja/releases/download/v0.0.33/ninja"
-  sha256 "4d06d931942232ad978ace6d635433d00dfec0e825090d7ddae2a15cfd60993c"
+  url  "https://github.com/gaberger/fwd-ninja/releases/download/v0.0.34/ninja"
+  sha256 "2044cd636ad04e02c87355c86b1f2b335eff66f18cb08f99fadf53fc97b68190"
 
   bottle :unneeded
 
