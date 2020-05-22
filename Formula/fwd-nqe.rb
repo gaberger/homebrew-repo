060cf929ad217b58269f2810704072b0110ed8b8
Gary <garyberger@forwardnetworks.com>
2020-05-22 10:23:57 -0400

    Update to fix limit
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-nqe.rb
+++ Formula/fwd-nqe.rb
@@ -1,7 +1,7 @@
 class FwdNqe < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/nqe"
-  url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.12/nqe"
-  sha256 "812f9bd92e5921f599c0cbe79d14ed34f8d871e377bbf4527404d4def61658e5"
+  url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.13/nqe"
+  sha256 "687cfe6682c9ad020d37ec521de96e356779fad56f14c67da5256b85e6b35020"
 
 
   bottle :unneeded
