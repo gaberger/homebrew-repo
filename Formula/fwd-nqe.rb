03372d6dd10032e4204a24022412fa2f8cbcc735
Gary <garyberger@forwardnetworks.com>
2020-05-21 20:29:37 -0400

    0.0.11
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fwd-nqe.rb
+++ Formula/fwd-nqe.rb
@@ -1,7 +1,7 @@
 class FwdNqe < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/nqe"
-  url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.10/nqe"
-  sha256 "a0eda76f22e3639afa1eb3cc426244ad947c29582249114f5ad527220e3c0f46"
+  url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.0.11/nqe"
+  sha256 "1b0bb1b7854e8a12123f29fac4ba9205d3658ace7b64ea3546148ecc266c8e56"
 
 
   bottle :unneeded
