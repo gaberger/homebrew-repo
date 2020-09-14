e79d05075b39b15979dbd8b5dfab8a6b0ca7fcf5
Gary <garyberger@forwardnetworks.com>
2020-09-14 18:00:39 -0400

    update for linux
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -3,19 +3,19 @@ require_relative "../custom_download_strategy.rb"
 class Ninja < Formula
   desc "Forward Ninja Admin Tool"
   version "v0.1.14"
-  url-select  = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
+  urlSelect  = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
-  url "#{url-select}" , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  url "#{urlSelect}" , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "3ca28b61172d16692ad8de0139887eb567a93cfecdd41c13ea48bc7f4f50747a"
 
 
   bottle :unneeded
-
+  
   def install
-    if url =~ /macos/i 
-      bin.install i"ninja-#{version}-macos" => "ninja"
+    if url =~ /macos/i
+      bin.install "ninja-#{version}-macos" => "ninja"
     else
-      bin.install i"ninja-#{version}-linux" => "ninja"
+      bin.install "ninja-#{version}-linux" => "ninja"
     end
   end
 
