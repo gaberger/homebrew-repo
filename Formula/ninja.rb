0462ac9c1b21a3d102b10cceb67fdf94530b589b
Gary <garyberger@forwardnetworks.com>
2020-09-14 18:55:28 -0400

    test os sel
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -5,7 +5,7 @@ class Ninja < Formula
   version "v0.1.14"
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
-  url "#{urlSelect}" , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  url #{urlSelect} , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "3ca28b61172d16692ad8de0139887eb567a93cfecdd41c13ea48bc7f4f50747a"
 
 
