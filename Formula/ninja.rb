85c951031c134209826f5e9d5c3405c57f99133b
Gary <garyberger@forwardnetworks.com>
2020-09-14 18:46:10 -0400

    test os sel
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -3,7 +3,7 @@ require_relative "../custom_download_strategy.rb"
 class Ninja < Formula
   desc "Forward Ninja Admin Tool"
   version "v0.1.14"
-  urlSelect  = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
+  urlSelect = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
   url "#{urlSelect}" , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "3ca28b61172d16692ad8de0139887eb567a93cfecdd41c13ea48bc7f4f50747a"
@@ -12,6 +12,7 @@ class Ninja < Formula
   bottle :unneeded
   
   def install
+    print OS.mac?
     if @url =~ /macos/i
       bin.install "ninja-#{version}-macos" => "ninja"
     else
