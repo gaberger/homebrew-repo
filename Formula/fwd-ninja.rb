08604d1f966fa771420b09ab53c72833d24287e3
Gary <garyberger@forwardnetworks.com>
2020-09-14 14:03:19 -0400

    Update for private repo handling

--- Formula/fwd-ninja.rb
+++ Formula/fwd-ninja.rb
@@ -1,7 +1,12 @@
+require_relative "../custom_download_strategy.rb"
+
 class FwdNinja < Formula
+  desc "Forward Ninja Admin Tool"
+  version "v0.1.14"
+  url  = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
-  url  "https://github.com/gaberger/fwd-ninja/releases/download/v0.1.8/ninja"
-  sha256 "29a2231d2fbb8c21283c9e2bd3beb713a51a486b1acf71cbd999ae479b10ba82"
+  url  ,  #{url} :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "1852ee59e2f87cf58247d64e728f7abdaed6ade7"
 
 
   bottle :unneeded
