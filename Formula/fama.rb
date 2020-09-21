43c79423f756bf35bf78c149203c09af4b70f368
Gary <garyberger@forwardnetworks.com>
2020-09-21 10:45:32 -0400

    1.10
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/fama.rb
+++ Formula/fama.rb
@@ -2,11 +2,11 @@ require_relative "../custom_download_strategy.rb"
 
 class Fama < Formula
   desc "Forward Ask Me Anythingl"
-  version "v0.1.9"
+  version "v0.1.10"
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/fama/releases/download/#{version}/fama-#{version}-macos" : "https://github.com/firstclassfunc/fama/releases/download/#{version}/fama-#{version}-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/fama"
   url urlSelect , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  sha = OS.mac? ? "d945051bfd400f8165cd6d5d87a867aa6cfaba329a44f92ccce59f3396cd170a" : "f553e74c10d8a6f60336a6d6cd5da5c2a33b272449abd416846a0786228c5607"
+  sha = OS.mac? ? "e394d999e6e6f779764993327b3eac8cf2673a2e2dafea2edb602fdaec280acf" : "6a7b6f525b9913d7cefc8bc6bad085a924e41956973831ba0af541937ed8518e"
   sha256 sha
 
   bottle :unneeded
