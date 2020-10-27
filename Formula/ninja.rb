55b41261bd4646742faa519097a83ad8450dd86a
Gary <garyberger@forwardnetworks.com>
2020-10-27 13:45:41 -0400

    bump ninja to 0.1.18
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -2,11 +2,11 @@ require_relative "../custom_download_strategy.rb"
 
 class Ninja < Formula
   desc "Forward Ninja Admin Tool"
-  version "v0.1.15"
+  version "v0.1.18"
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
   url urlSelect , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  sha = OS.mac? ? "1769484fbf31e6fe1be664ac04ef0349ab8f5f548e952a434c40436f05d68c88" : "e8d8db36aadc7b1ec76b385649d34c4927e8bf86d45424841b56edb624d139a4"
+  sha = OS.mac? ? "cc22339be58b9882252fc203b589658b3850160d62140692c31c34504d9069d8" : "e8d8db36aadc7b1ec76b385649d34c4927e8bf86d45424841b56edb624d139a4""
   sha256 sha
 
   bottle :unneeded
