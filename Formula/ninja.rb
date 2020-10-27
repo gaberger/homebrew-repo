0d689dbb9a3edddec98776b6193c7396c9529c42
Gary <garyberger@forwardnetworks.com>
2020-10-27 13:55:58 -0400

    Update

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -6,7 +6,7 @@ class Ninja < Formula
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-macos" : "https://github.com/firstclassfunc/ninja/releases/download/#{version}/ninja-#{version}-linux"
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
   url urlSelect , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  sha = OS.mac? ? "cc22339be58b9882252fc203b589658b3850160d62140692c31c34504d9069d8" : "e8d8db36aadc7b1ec76b385649d34c4927e8bf86d45424841b56edb624d139a4""
+  sha = OS.mac? ? "cc22339be58b9882252fc203b589658b3850160d62140692c31c34504d9069d8" : "e8d8db36aadc7b1ec76b385649d34c4927e8bf86d45424841b56edb624d139a4"
   sha256 sha
 
   bottle :unneeded
