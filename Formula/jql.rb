6258ef1c5b36b89fafbadaee0276b1aa105c9ad6
Gary Berger <garyberger@forwardnetworks.com>
2021-02-26 16:04:01 -0500

    update
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -7,7 +7,7 @@ class Jql < Formula
   urlSelect =  "https://github.com/firstclassfunc/jql/releases/download/#{version}/jql"
   url urlSelect, :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy  
   homepage "https://github.com/firstclassfunc/jql"
-  sha256 "fa511699d7bdae262677ccc1317ac8f150ba6b1c2be2873c030fb3b2c39ef96a"
+  sha256 "705bea82ecc5f9208a66976efc14dac2b90792308bcef0b0ba5ea36f6d3e7276"
 
   bottle :unneeded
 
