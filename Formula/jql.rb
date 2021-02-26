ccdfdcaa62fc063603a2b46c13973b8b1fbdf42a
Gary Berger <garyberger@forwardnetworks.com>
2021-02-26 13:18:45 -0500

    Update
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -4,9 +4,9 @@ class Jql < Formula
   desc "Forward JQL Tool"
   version "v0.0.8"
   depends_on "borkdude/brew/babashka"
-  url = "https://github.com/firstclassfunc/jql/blob/main/target/jql-#{version}.clj"
+  url "https://github.com/firstclassfunc/jql/blob/main/target/jql-#{version}.clj"
   homepage "https://github.com/firstclassfunc/jql"
-  sha = "fa511699d7bdae262677ccc1317ac8f150ba6b1c2be2873c030fb3b2c39ef96a"
+  sha256 "fa511699d7bdae262677ccc1317ac8f150ba6b1c2be2873c030fb3b2c39ef96a"
 
   bottle :unneeded
 
