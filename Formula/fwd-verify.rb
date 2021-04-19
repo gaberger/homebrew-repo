138f98dee03af3fce8a3cc9ec59e3522c2e2b6bc
Gary Berger <garyberger@forwardnetworks.com>
2021-04-19 11:25:56 -0400

    Update Version v0.0.8
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/fwd-verify.rb
+++ Formula/fwd-verify.rb
@@ -3,7 +3,7 @@ require_relative "../custom_download_strategy.rb"
 
 class FwdVerify < Formula
   desc "Forward Networks Ansible Verification Module"
-  version "v0.0.7"
+  version "v0.0.8"
   depends_on "borkdude/brew/babashka"
   urlSelect =  "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}"
   url urlSelect, :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy  
