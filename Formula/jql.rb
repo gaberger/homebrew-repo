64c719de6e4fb1e96bec64593227d72bdbf8ca5e
Gary Berger <garyberger@forwardnetworks.com>
2021-05-03 12:38:05 -0400

    Update Version v0.1.21
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -3,12 +3,12 @@ require_relative "../custom_download_strategy.rb"
 
 class Jql < Formula
   desc "Forward JQL Tool"
-  version "v0.1.22"
+  version "v0.1.21"
   depends_on "borkdude/brew/babashka"
   urlSelect =  "https://github.com/firstclassfunc/jql/releases/download/#{version}/jql"
   url urlSelect, :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy  
   homepage "https://github.com/firstclassfunc/jql"
-  sha256 "c2ca2df6d0a4231a9146f978df9912922a4e41283200ff820c5543f7c983838c"
+  sha256 "c2380b72b4e83c8d50beb759a8c62c306920e1872b3a66250c6b53cb6f4917a9"
 
   bottle :unneeded
 
