7588c5c439b6c52a3e4c11b7c2a7c83c2e5967aa
Gary Berger <garyberger@forwardnetworks.com>
2021-05-13 16:57:22 -0400

    Update Version v0.1.27
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -4,7 +4,7 @@ require "fileutils"
 
 class FwdVerify < Formula
   desc "Forward JQL"
-  version "v0.1.26"
+  version "v0.1.27"
   depends_on "borkdude/brew/babashka"
   urlSelect =   "https://github.com/firstclassfunc/jql/releases/download/#{version}/jql"
   url urlSelect, :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy  
