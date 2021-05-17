22313970ad030d4ce6f2b2c7d6985f93a5612d2d
Gary Berger <garyberger@forwardnetworks.com>
2021-05-17 16:48:33 -0400

    Update Version v0.0.1
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/sf.rb
+++ Formula/sf.rb
@@ -4,12 +4,12 @@ require "fileutils"
 
 class Jql < Formula
   desc "Forward SF"
-  version "v0.0.2"
+  version "v0.0.1"
   depends_on "borkdude/brew/babashka"
   urlSelect =   "https://github.com/firstclassfunc/sf/releases/download/#{version}/sf"
   url urlSelect, :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy  
   homepage "https://github.com/firstclassfunc/sf"
-  sha256 "712cd0f6850af28661138b41988e38d4a4a159b605d37555e0471649525e0bf5"
+  sha256 "f92e98e23b060ce963cdacc655ee02ca7b7bfe8f3a71bd0debe2b6cb9aa8a6e5"
 
   bottle :unneeded
 
