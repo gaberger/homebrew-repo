23ec30b69b247a9f171938641ead1db952f259b0
Gary Berger <garyberger@forwardnetworks.com>
2021-07-26 12:03:25 -0400

    fixing
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/fwd-verify.rb
+++ Formula/fwd-verify.rb
@@ -6,12 +6,12 @@ require_relative "lib/private_strategy"
 class FwdVerify < Formula
   desc "Forward Networks Ansible Verification Module"
   version "v0.0.18"
+  versionFile = OS.mac? ?  "fwd-verify-#{version}-macos" : "fwd-verify-#{version}-linux"                        
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}-macos" : "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}-linux"
   homepage "https://github.com/firstclassfunc/fwd-verify"
   url urlSelect , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha = OS.mac? ? "null" : "null"
   bottle :unneeded
-  versionFile = OS.mac? ?  "fwd-verify-#{version}-macos" : "fwd-verify-#{version}-linux"                        
 
   def install
     prefix.install "#{versionFile}"
