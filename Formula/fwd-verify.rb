108ad30ac89613ff8dcfbe05a6fc7938c04194ab
Gary Berger <garyberger@forwardnetworks.com>
2021-07-26 13:11:27 -0400

    final fix
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/fwd-verify.rb
+++ Formula/fwd-verify.rb
@@ -12,11 +12,9 @@ class FwdVerify < Formula
   url urlSelect , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha = OS.mac? ? "a2285fe35285aa08b64f879b46b649930d1ceb70576cc533eda15c669a9675e3" : "93e67ef9b817489677749b9c90d2f87a3c91d70012ae31d24876ab7ea567dd3b"
   bottle :unneeded
-
+  
   def install
-    bin.install @@versionFile => "fwd-verify"
-                     
-    #FileUtils.ln_s  "#{HOMEBREW_PREFIX}/Cellar/#{name}/#{version}/#{versionFile}", "#{HOMEBREW_PREFIX}/Cellar/#{name}/#{version}/fwd-verify", force: true
-  end
-                         
+    @versionFile = OS.mac? ?  "fwd-verify-#{version}-macos" : "fwd-verify-#{version}-linux"
+    bin.install @versionFile => "fwd-verify"
+  end              
 end
