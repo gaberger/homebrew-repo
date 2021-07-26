04c17dec1290e0335a3bdc48e6e77ecf6875b76e
Gary Berger <garyberger@forwardnetworks.com>
2021-07-26 09:40:55 -0400

    update script
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/fwd-verify.rb
+++ Formula/fwd-verify.rb
@@ -1,13 +1,14 @@
 
-require_relative "../custom_download_strategy.rb"
 require "fileutils"
+require "formula"
+require_relative "lib/private_strategy"
 
 class FwdVerify < Formula
   desc "Forward Networks Ansible Verification Module"
   version "v0.0.18"
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}-macos" : "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}-linux"
   homepage "https://github.com/firstclassfunc/fwd-verify"
-  url urlSelect , :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  url urlSelect , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha = OS.mac? ? "ffd5075a01692d29e4ae25dd00ded751217670a1017ef975a7b4c951fd9e7c3f" : "5e0be7bf21bcb582c295aee20998b7fde48b69f552d56e7a72c05b6dabcfb1e4"
   bottle :unneeded
 
