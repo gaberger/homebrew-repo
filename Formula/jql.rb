6b11e11953754da57814ec84015a8d010bcc0f1d
Gary Berger <garyberger@forwardnetworks.com>
2021-09-13 10:04:24 -0400

    update method
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -7,7 +7,7 @@ class Jql < Formula
   version "v0.1.32"
   depends_on "borkdude/brew/babashka"
   urlSelect =   "https://github.com/firstclassfunc/jql/releases/download/#{version}/jql"
-  url urlSelect, :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy  
+  url urlSelect , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   homepage "https://github.com/firstclassfunc/jql"
   sha256 "97cfd9dbd28a13ec91ffd907c6c3ee9fa82bff0bab6be5c6e84f68cec9dd1f80"
 
