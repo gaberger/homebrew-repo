ad12ac46469140862d1999e1565d88c5717c546c
Gary Berger <garyberger@forwardnetworks.com>
2021-12-29 09:17:04 -0500

    move formula to private repo
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/fwd-nqe.rb
+++ Formula/fwd-nqe.rb
@@ -1,9 +1,12 @@
+
+require "fileutils"
+require "formula"
+require_relative "lib/private_strategy"
+
 class FwdNqe < Formula
   homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/nqe"
   url  "https://github.com/gaberger/fwd-nqe/releases/download/v0.2.3/nqe"
   sha256 "774d5464626031d80a70f6124bf3df0328c9cfa2a47dcfb5c6adb8587fa55d31"
-
-
 
   def install
     bin.install "nqe"
