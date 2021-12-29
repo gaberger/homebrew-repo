269a598b1752244d18b1cdebfca61153d7cb5b11
Gary Berger <garyberger@forwardnetworks.com>
2021-12-29 09:10:33 -0500

    update to 256sha
    Signed-off-by: Gary Berger <garyberger@forwardnetworks.com>

--- Formula/fwd-verify.rb
+++ Formula/fwd-verify.rb
@@ -10,7 +10,7 @@ class FwdVerify < Formula
   urlSelect = OS.mac? ? "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}-macos" : "https://github.com/firstclassfunc/fwd-verify/releases/download/#{version}/fwd-verify-#{version}-linux"
   homepage "https://github.com/firstclassfunc/fwd-verify"
   url urlSelect , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha = OS.mac? ? "a2285fe35285aa08b64f879b46b649930d1ceb70576cc533eda15c669a9675e3" : "62dcc233ac264ee55153b8d068e9add54b440b65994c354f858ddcadaa7f9736"
+  sha256 = OS.mac? ? "a2285fe35285aa08b64f879b46b649930d1ceb70576cc533eda15c669a9675e3" : "62dcc233ac264ee55153b8d068e9add54b440b65994c354f858ddcadaa7f9736"
   
   def install
     @versionFile = OS.mac? ?  "fwd-verify-#{version}-macos" : "fwd-verify-#{version}-linux"
